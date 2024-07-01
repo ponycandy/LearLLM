1f3191be67578d93cc4008ad118c527bb24f2ca7
JudgeLevia <ponycandy666@126.com>
2024-07-01 16:38:54 +0800

    update

---
+++ AttentionMechanism/Multiattention_example.py
@@ -0,0 +1,24 @@
+import torch
+from MultiHeadAttentionWrapper import MultiHeadAttentionWrapper
+
+
+inputs = torch.tensor(
+    [[0.43, 0.15, 0.89], # Your     (x^1)
+     [0.55, 0.87, 0.66], # journey  (x^2)
+     [0.57, 0.85, 0.64], # starts   (x^3)
+     [0.22, 0.58, 0.33], # with     (x^4)
+     [0.77, 0.25, 0.10], # one      (x^5)
+     [0.05, 0.80, 0.55]] # step     (x^6)
+)
+
+
+batch = torch.stack((inputs, inputs), dim=0)
+
+torch.manual_seed(123)
+context_length = batch.shape[1]  # This is the number of tokens
+d_in, d_out = 3, 2
+mha = MultiHeadAttentionWrapper(d_in, d_out, context_length, 0.0, num_heads=2)
+context_vecs = mha(batch)
+
+print(context_vecs)
+print("context_vecs.shape:", context_vecs.shape)
